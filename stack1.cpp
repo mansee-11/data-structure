eb699d55c73a907af6b7c5d084f6834be2cf94a3
Mansee <silochanaasati30@gmail.com>
2025-11-18 22:22:01 +0530

    string palindrome using stack

--- stack1.cpp
+++ stack1.cpp
@@ -1,60 +1,48 @@
-#include <stdio.h>
-#include <conio.h>
-#include <iostream>
+#include<iostream>
 using namespace std;
-#define max 50
-
 struct stack{
-    char data[max];
+    string data;
     int top;
 };
-stack s1,*p1,s2,*p2; 
-
-void init()         //to initialise both stacks
+stack *p1,*p2,s1,s2;
+void init()
 {
     p1=&s1;
-    p2=&s2; 
+    p2=&s2;
     p1->top=-1;
     p2->top=-1;
 }
-
-void push2()    //to input data of stack 1 in stack 2 using lifo
+void push1(int a)
 {
-    int x=p1->top-1;   //p1->top will have '\0' so taking 1 less from top
-    int i=0;
-    for(x,i;x>-1;x--,i++)   //x is decresing from top-1 to -1 and i is increasing by 1 from 0
-    {
-        p2->top++;          //shifting top of 2nd stack
-        p2->data[i]=p1->data[x];   //pusing data in stack 2 reverse of stack 1
-    }
+    p1->top++;
+    p1->data+=a;
 }
-void palindrome()       // to check palindrome
+void push2(int a)
 {
-    int i;
-    for(i=0;i<p1->top;i++)
-    {
-        if((p1->data[i])!=(p2->data[i]))    //checking if same index having same char or not on reversing
-        {                                   
-            cout<<"not palindrome";         //if not same then print not palindrome
-            break;
-        }
-    }
-    if(p1->data[i]=='\0')   //to check if i reached end that means all char in stack1 and stack2 are same
-    {       
-        cout<<"palindrome";     
-    }
+    p2->top++;
+    p2->data+=p1->data[a];
 }
 int main()
 {
     init();
-    cout<<"enter a string to check palindrome:- ";
-    cin>>p1->data;      //diectly giving stack 1 data (string)
-    int i=0;
-    while(p1->data[i]!='\0')    //to get length of the string
+    string s;
+    cout<<"enter a string to check palindrome:-";
+    cin>>s;
+    int x=s.length();
+    for(int i=0;i<x;i++)
     {
-        i++;
+        push1(s[i]);
     }
-    p1->top=i;  //giving top of stack 1 its value (length of string)
-    push2(); 
-    palindrome();
+    for(int i=x-1,j=0;i>=0;i--,j++)
+    {
+        push2(i);
+    }
+    if(p1->data==p2->data)
+    {
+        cout<<"palindrome";
+    }
+    else
+    {
+        cout<<"not palindrome";
+    }
 }
