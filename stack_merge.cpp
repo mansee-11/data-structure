b97b7277bc16a144dfa666e4d924868cd2b5a589
Mansee <silochanaasati30@gmail.com>
2025-11-22 13:18:49 +0530

    merge two stack (alternative elements)

--- stack_merge.cpp
+++ stack_merge.cpp
@@ -10,14 +10,14 @@ void init()
 {
     top=top1=top2=temp=NULL;
 }
-void push(int x)   
+void push(int x)        //to push in main function
 {
-    temp=new stack;
-    temp->data=x;
-    temp->next=top;
-    top=temp;
+    temp=new stack;     //create new node       
+    temp->next=top;     //linking it to linked list
+    top=temp;           //increasing top (shifting)
+    top->data=x;        //pushing data 
 }
-void push1()   
+void push1()   //to push in 1st stack
 {
     int x;
     cin>>x;
@@ -26,7 +26,7 @@ void push1()
     temp->next=top1;
     top1=temp;
 }
-void push2()   
+void push2()    //to push in second stack
 {
     int x;
     cin>>x;
@@ -35,18 +35,18 @@ void push2()
     temp->next=top2;
     top2=temp;
 }
-int pop1()
+int pop1()     //to pop element from stack 1
 {
     int x;
-    if(top1==NULL)return -1;
-    temp=top1;
-    x=temp->data;
-    top1=top1->next;
-    temp->next=NULL;
-    delete temp;
-    return x;
+    if(top1==NULL)return -1;    //check if stack is empty
+    temp=top1;              
+    x=temp->data;           //taking the data of top node of stack
+    top1=top1->next;        //shifting back top(decreaing its position)
+    temp->next=NULL;        //removing it from linked list 
+    delete temp;            //deleting top node
+    return x;               //return data
 }
-int pop2()
+int pop2()      //to pop element of second stack
 {
     int x;
     if(top2==NULL)return -1;
@@ -57,7 +57,7 @@ int pop2()
     delete temp;
     return x;
 }
-void disp()
+void disp()     //to display the elements of merged stack
 {
     cout<<"data in merged stack:-"<<"\n";
     temp=top;
@@ -73,20 +73,20 @@ int main()
     init();
     cout<<"enter no of data in small stacks:- ";
     cin>>n;
-    cout<<"enter data for stack 1:-"<<"\n";
+    cout<<"enter data for stack 1:-"<<"\n";     //to push data in stack 1
     for(i=0;i<n;i++)
     {
         push1();
     }
-    cout<<"enter data for stack 2:-"<<"\n";
+    cout<<"enter data for stack 2:-"<<"\n";     //to push data in stack 2
     for(i=0;i<n;i++)
     {
         push2();
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++)    //pushing data in merged stack one by one from both stacks
     {
-        push(pop1());
-        push(pop2());
+        push(pop1());   //to push data in merged stack poped from 1st stack
+        push(pop2());   //to push data in merged stack poped from 2nd stack
     }
-    disp();
+    disp(); //to display elements of merged stack
 }
